6499876b2656bec3f50721ac750ffaa7af7e6447
Kstaples1 <kstapes@bu.edu>
2019-04-26 15:15:48 -0400

    update

---
+++ app/Http/Controllers/routesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class routesController extends Controller
+{
+    public function QueryBuilder()
+    {
+
+        return view('queryBuilder');
+
+    }
+}
